c39e2b1e3492cd5d8d1e8e9a550d3c534eb06ee2
AloraTab <aat2000@hw.ac.uk>
2023-06-14 15:27:39 +0400

    Small change

--- ivy/functional/frontends/numpy/statistics/histograms.py
+++ ivy/functional/frontends/numpy/statistics/histograms.py
@@ -2,8 +2,8 @@ import ivy
 
 def digitize(x, bins, right=False):
     if(right): 
-        side = 'right'
-    else:
         side = 'left'
+    else:
+        side = 'right'
     return ivy.searchsorted(bins, x, side = side)
     pass
